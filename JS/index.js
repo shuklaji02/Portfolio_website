3bf2aab0f8c98ec9894548f79e269b4192e7b38e
Bhaskar <75262936+shuklaji02@users.noreply.github.com>
2022-09-26 21:52:19 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,11 @@
 $(document).on("click", ".navLink .navItem", function () {
   $(this).addClass("active").siblings().removeClass("active");
 });
+
+// Removes Splash screen after 3 sec
 setTimeout(function () {
   $(".preload").remove();
+  document.body.classList.toggle("main");
 }, 3000);
 
 $(window).ready(function () {
